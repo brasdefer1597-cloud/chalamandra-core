0268036bce773bcc6658094b15dfa44db4db7de1
Dana Michelle Vargas de Brasdefer <brasdefer1597@hotmail.com>
2025-10-17 18:24:04 -0600

    refactor: update detection method names

--- core/sarcasm-detector.js
+++ core/sarcasm-detector.js
@@ -173,8 +173,4 @@ class SarcasmDetector {
       return this.heuristicVisualAnalysis({});
     }
   }
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = SarcasmDetector;
-      }
+                      }
